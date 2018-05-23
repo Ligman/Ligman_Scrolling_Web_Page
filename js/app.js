b2565f34336655a59f44fc4ac55662484923476c
Art 3870-02 <art3870-02@Arts-194-109.local>
2018-05-23 15:26:24 -0700

    22

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-$(document).foundation()
+var canvas = document.getElementById("canvas");
+var counter = document.getElementById("counter");
+var icon = document.getElementById("icon");
+
+window.onscroll = function(){
+
+
+  var y = window.pageYOffset;
+
+  console.log(y);
+
+  counter.style.top = 400 + y * .97 + "px";
+
+  counter.innerHTML = y + "px";
+
+  if (y > 2000) {
+    counter.style.backgroundColor = "green";
+  }else{
+    counter.style.backgroundColor = "blue";}
+
+    icon.style.top = -6800 + y * 1.08 + "px";
+    icon.innerHTML = y + "px";
+
+};
